210815c25842516f8d226988c5f3535d55c9b896
Aayushjoshi28 <aayushj8355@gmail.com>
2021-11-10 09:32:54 +0530

    .

--- assets/js/z.js
+++ assets/js/z.js
@@ -1,4 +1,5 @@
 function recheck() {
+
     if (userdata === "http://www.vascot.tech") {
         sqlivalue = 0;
         xssvalue = 0;
@@ -13,12 +14,21 @@ function recheck() {
         portvalue = 96;
     }
     else{
-        if (op1===1){
-
+        if (op1==1){
+            const ln1 = [50,100];
+            const random = Math.floor(Math.random() * ln1.length);
+            sqlivalue = ln1[random];
         }
-        const ln1 = [50,100];
-        const random = Math.floor(Math.random() * ln1.length);
-        console.log(random, ln1[random]);
+        if (op2==1){
+            const ln2 = [50,100];
+            const random2 = Math.floor(Math.random() * ln2.length);
+            xssvalue = ln2[random2];
+        }
+        if (op3==1){
+            const ln3 = [50,62,84,96];
+            const random3 = Math.floor(Math.random() * ln3.length);
+            portvalue = ln3[random3];
+        }
     }
 }
 function sqli() {
@@ -33,17 +43,26 @@ function pscan() {
 }
 
 function score() {
-    factor = 2;
-    if (factor === 1) {
+    factor = 0
+    factor = 3-(op1+op2+op3)
+    if (factor == 0){
+        outof = 300
+    }
+    if (factor == 1) {
         outof = 200;
     }
-    if (factor === 2) {
+    if (factor == 2) {
         outof = 100;
+    }
+    if (factor == 3) {
+        outof = 10;
     }
     sum = sqlivalue + xssvalue + portvalue;
     scorevalue = (sum * 10) / outof
     document.getElementById('score').textContent = scorevalue;
 }
+
+
 
 function values() {
     recheck();
